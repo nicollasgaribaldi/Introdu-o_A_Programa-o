6b2ffbfcd44ca86e58b4207a7f682df2db858096
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2023-10-16 21:40:23 -0300

    exercicios

---
+++ Questao14.py
@@ -0,0 +1,14 @@
+from random import randint
+
+lista = []
+resultado_da_soma = 0
+
+for n in range(10):
+    lista.append(randint(1, 30))
+
+for i in lista:
+    if (i % 2) == 0:
+        resultado_da_soma = resultado_da_soma + i
+
+print(f'O resultado da soma é: {resultado_da_soma}')
+print(lista)
